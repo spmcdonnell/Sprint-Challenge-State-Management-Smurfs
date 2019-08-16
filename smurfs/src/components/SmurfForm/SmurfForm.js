36bdbbd75c8ef30b5f93cdf0e745a7f0c343cd52
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-08-16 12:38:49 -0400

    Add formik with post functionality to add a smurf to the server and complete MVP

--- smurfs/src/components/SmurfForm/SmurfForm.js
+++ smurfs/src/components/SmurfForm/SmurfForm.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
-function SmurfForm() {
-    return <div>Add new smurf</div>;
+function SmurfForm({ values, errors, touched, isSubmitting }) {
+    return (
+        <div className="login-form">
+            <Form>
+                <div>
+                    <label>
+                        Name:
+                        <Field name="name" type="text" placeholder="Name" />
+                        {touched.name && errors.name && <span style={{ color: 'red' }}>{errors.name}</span>}
+                    </label>
+                    <label>
+                        Age:
+                        <Field name="age" type="number" placeholder="Age" />
+                        {touched.age && errors.age && <span style={{ color: 'red' }}>{errors.age}</span>}
+                    </label>
+                    <label>
+                        Name:
+                        <Field name="height" type="text" placeholder="Height" />
+                        {touched.height && errors.height && <span style={{ color: 'red' }}>{errors.height}</span>}
+                    </label>
+                </div>
+                <button type="submit" disabled={isSubmitting}>
+                    Submit
+                </button>
+            </Form>
+        </div>
+    );
 }
 
-export default SmurfForm;
+const FormikSmurfForm = withFormik({
+    mapPropsToValues({ name, age, height }) {
+        return {
+            name: name || '',
+            age: age || '',
+            height: height || ''
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required('Name is required'),
+        age: Yup.number().required('Age is required'),
+        height: Yup.string().required('Height is required')
+    }),
+    handleSubmit(values, { props, resetForm, setErrors, setSubmitting }) {
+        axios
+            .post('http://localhost:3333/smurfs', values)
+            .then(res => {
+                console.log(res); // Data was created successfully and logs to console
+                resetForm();
+                setSubmitting(false);
+            })
+            .catch(err => {
+                console.log(err); // There was an error creating the data and logs to console
+                setSubmitting(false);
+            });
+    }
+})(SmurfForm);
+
+export default FormikSmurfForm;
